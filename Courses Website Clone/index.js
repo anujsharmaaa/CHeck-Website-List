3bd4687a5395922e16700a8d8042533497656090
anujsharmaaa <93985569+anujsharmaaa@users.noreply.github.com>
2022-11-15 21:24:54 +0530

    Add files via upload

---
+++ Courses Website Clone/index.js
@@ -0,0 +1,65 @@
+// change navbar style on scroll 
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0 )
+})
+
+// show and hide in faq ans 
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        // Change Icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus' ){
+            icon.className = "uil uil-minus"
+        } else{
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
+
+// show / hide nav menu 
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+
+})
+
+
+// close nav menu 
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none"
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav )
+
+
+
+// swiper start //
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // when window width is >= 600px   
+    breakpoints: {
+        600: {
+          slidesPerView: 2,
+        }
+    }
+  });
+// swiper end //
